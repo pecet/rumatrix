32f3224de2e6d85d91807382d60449a41f83de43
Piotr Czarny <pecet1@gmail.com>
2022-10-22 22:03:45 +0200

    Something which should work, but it doesn't (maybe stdout buffering?)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
 use rand::prelude::*;
 use termion::{color, style, clear, cursor, screen, terminal_size};
+use std::{thread, time::Duration};
+
+#[derive(Debug, Clone, Copy)]
+struct Position {
+    x: u16,
+    y: u16,
+}
+#[derive(Debug)]
+struct FallingChar {
+    position: Position,
+    //previous_position: Position,
+    max_position: Position,
+    char_to_render: char,
+    fg: &'static str,
+}
+
+impl FallingChar {
+    fn new(max_x: u16, max_y: u16) -> Self {
+        Self {
+            position: Position { x: thread_rng().gen_range(1..max_x), y: 1 },
+            max_position: Position { x: max_x, y: max_y },
+            char_to_render: '#',
+            fg: random_fg(),
+        }
+    }
+
+    fn out_of_bounds(&self) -> bool {
+        self.position.y > self.max_position.y || self.position.x > self.max_position.x
+    }
+
+    fn render(&self) {
+        if !self.out_of_bounds() {
+            print!("{}{}{}", cursor::Goto(self.position.x, self.position.y), self.fg, self.char_to_render);
+        }
+    }
+
+    fn advance(&mut self) {
+        if !self.out_of_bounds() {
+            self.position.y += 1;
+        }
+    }
+}
 
 fn random_fg() -> &'static str {
     let rand_value = thread_rng().gen_range(2..=8);
@@ -16,15 +58,19 @@ fn random_fg() -> &'static str {
     color
 }
 
+fn main_loop(falling_chars: &mut Vec<FallingChar>) {
+    for f in falling_chars {
+        f.render();
+        f.advance();
+    }
+    thread::sleep(Duration::from_millis(100));
+}
+
 fn main() {
     let (size_x, size_y) = terminal_size().expect("Cannot get terminal size!");
+    let mut falling_chars = vec![FallingChar::new(size_x, size_y), FallingChar::new(size_x, size_y), FallingChar::new(size_x, size_y)];
     print!("{}", clear::All);
-    
-    for x in 1..=size_x {
-        for y in 1..=size_y {
-            print!("{}", cursor::Goto(x, y));
-            print!("{}#", random_fg());
-        }
+    loop {
+        main_loop(&mut falling_chars);
     }
-
 }
