f58924a5a6365cb4b8572f16a6bdedb4d9149e97
Piotr Czarny <926143+pecet@users.noreply.github.com>
2022-10-30 20:57:31 +0100

    Short form for added cli params

--- src/cli_parser.rs
+++ src/cli_parser.rs
@@ -7,10 +7,10 @@ use clap::{Parser, Subcommand};
 #[command(about = "cmatrix inspired program but in Rust", long_about = None)]
 pub struct Cli {
     /// Force width (x) of the screen
-    #[arg(long)]
+    #[arg(long, short='x')]
     pub size_x: Option<u16>,
 
     /// Force height (y) of the screen
-    #[arg(long)]
+    #[arg(long, short='y')]
     pub size_y: Option<u16>,
 }
